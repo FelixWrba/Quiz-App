611e95e8584485157e8990d33d0acc38800a3d3d
Felix Wrba <felix.wrba@web.de>
2024-10-28 21:35:58 +0100

    Validation added

--- routes/account.js
+++ routes/account.js
@@ -35,12 +35,44 @@ router
     })
     .post(async (req, res) => {
         const { name, email, password } = req.body;
-        try {
-            const newUser = await User.create({ name, email, password });
-            res.json({ message: 'User created!', user: newUser });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
+
+        // Validation
+        if (!isValid(name, 3, 12, false)) {
+            res.send('Name must be 3-12 characters long and only conatin letters and underscores.');
+            return;
         }
+        if (!isValid(password, 8, 16, true)) {
+            res.send('Password must be 8-16 characters long and cannot contain spaces');
+            return;
+        }
+        if (!(isValid(email, 4, 64, true) && email.includes('@'))) {
+            res.send('Email must be 4-64 characters long and contain an @ symbol');
+            console.log(email.includes('@'));
+            return;
+        }
+        res.send('Input valid')
+
+        // try {
+        //     const newUser = await User.create({ name, email, password });
+        //     res.json({ message: 'User created!', user: newUser });
+        // } catch (error) {
+        //     res.status(500).json({ error: error.message });
+        // }
     });
+
+function isValid(string, min, max, extend) {
+    // check if length within specified bounds
+    if (string.length < min || string.length > max) {
+        return false;
+    }
+    // Check if contains allowed characters
+    if (!extend && !/^[A-Za-z_]+$/.test(string)) {
+        return false;
+    }
+    if (extend && /\s/.test(string)) {
+        return false;
+    }
+    return true;
+}
 
 module.exports = router;
